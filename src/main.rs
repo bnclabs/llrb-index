4224bf391794b95c5b0ffe4a78526a25678f9cf4
prataprc <prataprc@gmail.com>
2019-02-17 08:14:31 +0530

    cosmetics

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,3 @@
-// Performance measurement for Llrb instances. Measure:
-// * Initial load, 1M and 10M and 100M
-// * Set operation, overwrite 1M data set, 10M and 100M data set
-// * Delete operation 10M -> 1M and 100M -> 10M
-// * Get operation all 1M, all 10M, all 100M
-//
-// For each data set measure latency and throughput
-// * For every 10th operation measure the latency.
-// * Compute the total time taken to complete all the operation for set.
-// * Tally, latency * num_op = throughtput
-//
-// Repeat the above process for:
-// * {key=i64, value=i64}
-// * {key=String(32), value=String(256)}
-//
-// Precreate the set before applying the operation.
-
 fn main() {
     unreachable!()
 }
