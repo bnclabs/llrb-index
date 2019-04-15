5260063514e2f83ddd5af24b53d011414888676e
prataprc <prataprc@gmail.com>
2019-04-15 11:25:40 +0530

    refactor depth.

--- src/depth.rs
+++ src/depth.rs
@@ -11,13 +11,8 @@ pub struct Depth {
 
 impl Depth {
     pub(crate) fn new() -> Depth {
-        let mut depth = Depth {
-            samples: 0,
-            min: 0,
-            max: 0,
-            total: 0,
-            depths: Vec::with_capacity(256),
-        };
+        let mut depth: Depth = Default::default();
+        depth.depths = Vec::with_capacity(256);
         depth.depths.resize(256, 0);
         depth
     }
@@ -44,26 +39,26 @@ impl Depth {
         self.min
     }
 
-    /// Return the average depth of leaf-nodes in LLRB tree.
-    pub fn mean(&self) -> usize {
-        self.total / self.samples
-    }
-
     /// Return maximum depth of leaf-node in LLRB tree.
     pub fn max(&self) -> usize {
         self.max
     }
 
+    /// Return the average depth of leaf-nodes in LLRB tree.
+    pub fn mean(&self) -> usize {
+        self.total / self.samples
+    }
+
     /// Return depth as tuple of percentiles, each tuple provides
-    /// (percentile, depth).
+    /// (percentile, depth). Returned percentiles from 90, 91 .. 99
     pub fn percentiles(&self) -> Vec<(u8, usize)> {
         let mut percentiles: Vec<(u8, usize)> = vec![];
-        let (mut acc, mut prev_perc) = (0_f64, 89_u8);
+        let (mut acc, mut prev_perc) = (0_f64, 90_u8);
         let iter = self.depths.iter().enumerate().filter(|(_, &item)| item > 0);
         for (depth, samples) in iter {
             acc += *samples as f64;
             let perc = ((acc / (self.samples as f64)) * 100_f64) as u8;
-            if perc > prev_perc {
+            if perc >= prev_perc {
                 percentiles.push((perc, depth));
                 prev_perc = perc;
             }
